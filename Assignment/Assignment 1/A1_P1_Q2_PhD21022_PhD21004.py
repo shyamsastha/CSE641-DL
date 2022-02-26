5ca04141d74facbd97d4998d7a993be4ad9d3321
Shyama Sastha Krishnamoorthy Srinivasan <shyamas@iiitd.ac.in>
2022-02-26 18:10:14 +0530

    Update A1_P1_Q2_PhD21022_PhD21004.py
    Updating code for A1_P1_Q2.

--- Assignment/Assignment 1/A1_P1_Q2_PhD21022_PhD21004.py
+++ Assignment/Assignment 1/A1_P1_Q2_PhD21022_PhD21004.py
@@ -46,69 +46,78 @@ def activation(y):
     if y >= 0:
       return 1
     else:
-      return 0
+      return -1
 
 # Defining the function to create a Madaline network with random weights
 def init_mn(xn, hn1, hn2, yn):
     mn = []
-    hl1 = [{'w':[random() for i in range(xn + 1)]} for i in range(hn1)]
+    hl1 = [{'w':[random() for i in range(xn+1)], 'i': int(i/4) , 'f': 0, 'l': 0} for i in range(hn1)]
     mn.append(hl1)
-    hl2 = [{'w':[random() for i in range(hn1 + 1)]} for i in range(hn2)]
+    hl2 = [{'w':[random() for i in range(hn2+1)], 'i': i, 'f': 0, 'l': 1} for i in range(hn2)]
     mn.append(hl2)
     # setting the output layer weights as 1
-    yl = [{'w':[1 for i in range(hn2 + 1)]}]
+    yl = [{'w':[random() for i in range(hn2+1)], 'i': 1, 'l': 2, 'f': 0}]
     mn.append(yl)
     return mn
 
 # Defining the function to create net input  for each neuron
-def zij(w, x):
-    z = w[-1] #the final random weight is chosen as bias
-    for i in range(len(w)-1):
-        z += w[i] * x[i]
+def zij(n, x):
+    w = n['w']
+    z = w[-1] # The bias is always set as 1
+    for j in range(len(w)-1):
+        z += w[j] * x[j]
     return z
 
 # Defining the function for forward propagation of weights
 def propf(mn, row):
-    x = row
+    x = row[:-1]
     for i in range(len(mn)):
         l = mn[i]
         nlx = [] #input for next layer
         for n in l:
-            n['z'] = zij(n['w'], x)
-            n['o'] = activation(n['z'])
-            nlx.append(n['o'])
+            if n['f'] != 1:
+                n['z'] = zij(n, x,)
+                n['o'] = activation(n['z'])
+                nlx.append(n['o'])
+            else:
+                nlx.append(n['o'])
         x = nlx
     return x
 
-#Defining the function for finding the neuron with smallest z value
-def zmin(l, z):
-    for n in l:
-        if z == n['z']:
-            return n
+# Defining the function to identify the neuron with smallest affine z value and flip its output
+def zsafv(mn, row, d):
+    flag = 0
+    flipback(mn)
+    while flag != 1:
+        lzm = []
+        for l in mn:
+            lzm = [n for n in l if n['f'] != 1]
+        mm = min(lzm, key=lambda x:x['z'])
+        for n in mn[mm['l']]:
+            if n['z'] == mm['z']:
+                n['o'] = -1 * n['o']
+                n['f'] = 1
+        flag = (propf(mn, row)[0] == d)
+    return mm
+
+
+# Defining the function to flip back all the flag values
+def flipback(mn):
+    for i in range(len(mn)):
+        for n in mn[i]:
+            n['f'] = 0
 
 # Defining the function for Updating weights
-def updatew(mn, row, lr):
-    for i in reversed(range(len(mn)-1)):
-        j = i-1
-        x = row[:-1]
-        d = row[-1]
-        for n in mn[i]:
-            if d != n['o']:
-                z_min = min(mn[j], key=lambda x:x['z'])
-                m = zmin(mn[j], z_min['z'])
-                if j != 0:
-                    x = [n['o'] for n in mn[j]]
-                for k in range(len(x)):
-                    m['w'][k] += lr * n['del'] * x[k]
-                m['w'][-1] += lr * n['del']
-
-# Defining the function for backward propagation of errors
-def propb(mn, d):
-    for i in reversed(range(len(mn)-1)):
-        l = mn[i]
-        for j in range(len(l)):
-            n = l[j]
-            n['del'] = (d - n['z'])
+def updatew(mn, row, lr, d):
+    n = zsafv(mn, row, d)
+    i = n['l']
+    x = row[:-1]
+    if n['l'] != 1:
+        x = [n['o'] for n in mn[i-1]]
+    for k in range(len(x)):
+        n['w'][k] += lr * (d - n['z']) * x[k]
+        n['w'][-1] += lr * (d - n['z'])
+    print('Weights updated successfully')
 
 # Defining the function to train the Madaline network for a nE epochs
 def train_mn(mn, ddata, lr, nE, yn):
@@ -117,30 +126,31 @@ def train_mn(mn, ddata, lr, nE, yn):
         for row in data:
             output = propf(mn, row)[0]
             d = row[-1]
-            E += (d-output)**2
-            propb(mn, d)
-            updatew(mn, row, lr)
-        print('-> epoch= {}, lrate= {}, Error= {}, Predicted= {}'.format(epoch, lr, E, output))
+            E += 0.5 * (d-output)**2
+            if d != output:
+                updatew(mn, row, lr, d)
+    print('\n')
+    print('-> Final epoch= {}, learning rate= {}, Error= {}, Desired = {}, Predicted= {}'.format(epoch+1, lr, E, d, output))
     print('\n')
 
 # Sample inputs for training
-data = [[1, 1, 0], [3, 3 ,0],
-        [5, 1, 1], [5.5, 1.5, 1],
-        [5, 3, 0], [5.5, 3.5, 0],
-        [7, 3, 0], [9, 1, 0],
+data = [[5, 1, 1], [5.5, 1.5, 1],
+        [1, 1, -1], [3, 3 ,-1],
+        [5, 3, -1], [5.5, 3.5, -1],
+        [7, 3, -1], [9, 1, -1],
         [1, 5, 1], [1.5, 5.5, 1],
-        [3, 5, 0], [3.5, 5.5, 0],
+        [3, 5, -1], [3.5, 5.5, -1],
         [5, 5, 1], [5.5, 5.5, 1],
-        [7, 5, 0], [7.5, 5.5, 0],
+        [7, 5, -1], [7.5, 5.5, -1],
         [9, 5, 1], [9.5, 5.5, 1],
-        [3, 7, 0], [1, 9, 0],
-        [5, 7, 0], [5.5, 7.5, 0],
+        [3, 7, -1], [1, 9, -1],
+        [5, 7, -1], [5.5, 7.5, -1],
         [5, 9, 1], [5.5, 9.5, 1],
-        [7, 7, 0], [9, 9, 0]]
+        [7, 7, -1], [9, 9, -1]]
 xn = len(data[0]) - 1
 yn = 1
-mn = init_mn(xn, 12, 5, yn)
-train_mn(mn, data, 0.1, 10000, yn)
+mn = init_mn(xn, 16, 4, yn)
+train_mn(mn, data, 0.1, 1000, yn)
 for l in mn:
     print(l)
     print("\n")
@@ -157,29 +167,5 @@ This means a minimum of 6 vertical boundaries and 6 horizontal boundaries are re
 Then to actually figure out the bounds for each square, 5 more neurons are needed.
 Because 4 of the 12 neurons from the first layer will bound each box through AND condition and there are 5 boxes.
 And to Sum it all up, a neuron is required to make up for the OR condition from the 5 outputs to get a final outcome.
-"""
 
-
-"""
-# Initial idea for Adaline.
-# First for every neuron in the last layer
-# Then process them layer by layer
-# Till you reach the first layer
-
-# Defining function for backpropagation of error for one neuron
-def Perceptron(X, W, b, d):
-    # Initializing weights
-    max =100
-    eta = 0.1
-    itr = 1
-    error = 1
-    z = np.dot(W, X) + b # This part can be made into a function
-    while (itr < max and error == 1):
-        for i in range(len(X)):
-            for j in range(len(W)):
-                y = activation(z)
-                if y != d:
-                    # using weight update rule of Adaline
-                    W[i][j] = W[i][j] + (eta * (d - z) * X[i])
-                    b[i][j] = b[i][j] + (eta * (d - z))
 """
